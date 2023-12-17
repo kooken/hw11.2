0049923b36b93925d9a6cb2a79aa4dd8dd631dfa
mariasazhina <msazhina23@gmail.com>
2023-12-17 18:29:41 +0300

    func changes

--- str_func.py
+++ str_func.py
@@ -3,9 +3,4 @@ def capital_letters(word):
     word_in_capital = [i.upper() for i in word_list]
     print("".join(word_in_capital))
 
-#capital_letters(input())
-
-def up_string(input):
-    return input.upper()
-
-up_string("word")
+capital_letters(input())
