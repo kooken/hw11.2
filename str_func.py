f84a183fa4f560514fea8f4142c31e9aa7daab42
kooken <153303506+kooken@users.noreply.github.com>
2023-12-17 18:42:49 +0300

    Update str_func.py
    updated func

--- str_func.py
+++ str_func.py
@@ -1,6 +1,5 @@
 def capital_letters(word):
+    '''описание функции'''
     word_list = list(word)
     word_in_capital = [i.upper() for i in word_list]
     print("".join(word_in_capital))
-
-capital_letters(input())
