9ed6d686bc4631ea5f634236c6832f5c63617907
mariasazhina <msazhina23@gmail.com>
2023-12-17 18:59:22 +0300

    docstrings added

--- str_func.py
+++ str_func.py
@@ -1,6 +1,10 @@
 def capital_letters(word):
+    '''докстринг к старой функции'''
     word_list = list(word)
     word_in_capital = [i.upper() for i in word_list]
     print("".join(word_in_capital))
 
-capital_letters(input())
+def title_letters(word):
+    '''докстринг к новой функции'''
+    word_title = word.title()
+    print(word_title)
